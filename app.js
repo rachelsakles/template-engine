6fc32bbf7787efb3e77dd28499fc3c516642f5b1
Rachel Sakles <rachel.sakles@yahoo.com>
2020-09-02 10:12:08 -0700

    added employee js

--- app.js
+++ app.js
@@ -1,105 +1,80 @@
-// Node modules
+// Node Modules
 const path = require("path");
 const fs = require("fs");
 
+// NPM Modules
+const inquirer = require("inquirer");
+
+// Constructors
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-
-const inquirer = require("inquirer");
+const Intern = require("./lib/Intern"); 
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./lib/htmlRenderer"); 
+const { listenerCount } = require("process");
 
-const render = require("./lib/htmlRenderer");
+const employees = []; 
+const {
+    managerPrompt,
+    addEmployee,
+    internPrompt,
+    engineerPrompt,
+  } = require("./lib/prompts");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!) 
-inquirer.prompt([
-    {
-        message: "What is your manager's name?",
-        name: "name"
-    },
-    {
-        message: "What is your manager's id?",
-        name: "id"
-    },
-    {
-        message: "What is your manager's email address?",
-        name: "email"
-    },
-    {
-        message: "What is your manager's office number?",
-        name: "officeNumber"
-    },
-]).then(function (manager) {
-    console.log(manager);
-    inquirer
-        .prompt([
-            {
-                type: "list",
-                message: "Which type of team member would you like to add?",
-                name: "engineerOrIntern",
-                choices: ["Engineer", "Intern", "Quit"],
-            }
-        ])
-        .then(function ({ engineerOrIntern }) {
-            if (engineerOrIntern === "Engineer") {
-                // prompt for engineer details 
-                engineerPrompt();
-            } else if (engineerOrIntern === "Intern") {
-                // prompt for intern details 
-                internPrompt();
-            } else {
-                // write to file
 
-            }
-        })
-    });
-function engineerPrompt() {
-    inquirer
-        .prompt([
-            {
-                message: "What is your engineer's name?",
-                name: name
-            },
-            {
-                message: "What is your engineer's id?",
-                name: "id"
-            },
-            {
-                message: "What is your engineer's email address?",
-                name: "email"
-            },
-            {
-                message: "What is your engineer's GitHub username?",
-                name: "officeNumber"
-            },
-        ])
-}
-function internPrompt() {
-    inquirer
-        .prompt([
-            {
-                message: "What is your intern's name?",
-                name: name
-            },
-            {
-                message: "What is your intern's id?",
-                name: "id"
-            },
-            {
-                message: "What is your intern's email address?",
-                name: "email"
-            },
-            {
-                message: "What is your intern's school?",
-                name: "officeNumber"
-            },
-        ])
-}
+managerPrompt().then(function (manager) {
+    // builds manager, pushes to array
+    employees.push(
+      new Manager(manager.name, manager.id, manager.email, manager.officeNumber)
+    );
+    main();
+  });
 
+
+  function main () { 
+      addEmployee().then(function ({ engineerOrIntern }) {
+          if(engineerOrIntern === "Engineer") {
+              // prompt for engineer details 
+              engineerPrompt().then (function (engineer) { 
+                  employees.push( 
+                      new Engineer( 
+                          engineer.name,
+                          engineer.id, 
+                          engineer.email,
+                          engineer.github
+                      )
+                  ); 
+                  main();
+              });
+          } else if(engineerOrIntern === "Intern") {
+              // prompt for intern details 
+              internPrompt().then (function (intern){ 
+                  employees.push( 
+                      new Intern( 
+                          intern.name, 
+                          intern.id, 
+                          intern.email, 
+                          intern.school 
+                      )
+                  ); 
+                  main();
+              });
+          } else {
+              // write to file 
+              fs.writeFile(outputPath, render(employees), (err) => { 
+                  if (err) throw err; 
+                  console.log("File has successfully been written");
+              });
+  
+          }
+      });
+  
+  }
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
